85ef62f31668e24a0a903a6daedee63635629df9
SamarV-121 <samarvispute121@gmail.com>
2017-10-01 18:26:49 +0530

    nicklaus: Initial Trees

---
+++ lineage.mk
@@ -0,0 +1,17 @@
+## Specify phone tech before including full_phone
+
+# Release name
+PRODUCT_RELEASE_NAME := nicklaus
+
+# Inherit some common CM stuff.
+$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+
+# Inherit device configuration
+$(call inherit-product, device/motorola/nicklaus/nicklaus.mk)
+
+# Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := nicklaus
+PRODUCT_NAME := lineage_nicklaus
+PRODUCT_BRAND := Motorola
+PRODUCT_MODEL := Moto E4 Plus
+PRODUCT_MANUFACTURER := Motorola
